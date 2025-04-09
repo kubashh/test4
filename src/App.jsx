84e852af4dea89703a7bd2e07635befa8ad2efe2
Jakub <jakubhanula0@gmail.com>
2025-04-09 18:21:10 +0200

    0.0.0.2

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
+import { useParams } from "react-router-dom"
 import { Navbar } from "./components/Navbar"
 
+const arr = [{ id: 1, slug: `aaa` }]
+
 function App() {
+  const { slug } = useParams()
+
+  const item = arr.find((x) => x.slug === slug)
+
   return (
     <>
       <Navbar />
